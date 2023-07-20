41907848305fe75faaa05297935bdfe21f13dd21
Kevin Caldwell <kevinc2004@gmail.com>
2023-07-20 19:43:30 -0400

    Remove Build Files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kevin/Kevin/Programming/C/CCSDS_Compression/C
-# Build directory: /home/kevin/Kevin/Programming/C/CCSDS_Compression/C/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
