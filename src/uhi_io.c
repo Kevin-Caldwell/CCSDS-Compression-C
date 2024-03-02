e1c180ff92366644f838f454543a54d709c41590
Lanlin He <lanlin.he@mail.utoronto.ca>
2024-03-02 14:44:38 -0500

    Finished functions in uhi-io.c, to be tested

--- src/uhi_io.c
+++ src/uhi_io.c
@@ -1,57 +1,52 @@
 #include "files/uhi_io.h"
+
+#include <stdio.h>
+
 #include "dst/image.h"
 
-int UHI_Initialize(
-    UHI* stream, 
-    dim3 buffer_size,
-    const char* file_name, 
-    FileMode file_mode
-){
-    stream->fs = F_OPEN(file_name, READ);
-    // TODO Initialize Full File
-    stream->cache[0] = 0;
+int UHI_Initialize(UHI* stream, dim3 buffer_size, const char* file_name,
+                   FileMode file_mode) {
+  file_t* file = F_OPEN(file_name, file_mode);
+  stream->fs = file;
+  if(stream->fs == NULL){
     return 0;
+  }
+  // TODO Initialize Full File
+  stream->size = buffer_size;
+  stream->cache[0] = 0;
+  return 0;
 }
-
-int UHI_WriteImage(
-    const char* filename, 
-    const image* source
-) {
-
-    return 0;
-}
-
 
 // | S S | S S | S S | X X | X X |
 //    X     Y     Z     0     1
-PIXEL UHI_ReadPixel(
-    UHI* stream, 
-    dim3 index
-) {
-    PIXEL pixel;
-    dim3 s = stream->size;
-    F_SEEK(stream->fs, stream->cache[6 + 2 * (s.x * s.y * index.z + s.x * index.y + index.x)]);
-    F_READ(&pixel, sizeof(PIXEL), 1, stream->fs);
-    return 0;
+PIXEL UHI_ReadPixel(UHI* stream, dim3 index) {
+  PIXEL pixel;
+  dim3 s = stream->size;
+  F_SEEK(
+      stream->fs,
+      stream->cache[6 + 2 * (s.x * s.y * index.z + s.x * index.y + index.x)]);
+  F_READ(&pixel, sizeof(PIXEL), 1, stream->fs);
+  return 0;
 }
 
-int UHI_WritePixel(
-    const UHI* stream, 
-    dim3 index,
-    PIXEL value
-){
-    // Replace with Code
+int UHI_WritePixel(const UHI* stream, dim3 index, PIXEL value) {
+  dim3 s = stream->size;
+  F_SEEK(
+      stream->fs,
+      stream->cache[6 + 2 * (s.x * s.y * index.z + s.x * index.y + index.x)]);
+  F_WRITE(&value, sizeof(PIXEL), 1, stream->fs);
+  return 0;
 }
 
-int UHI_close(UHI* stream){
-    F_CLOSE(stream->fs);
+int UHI_close(UHI* stream) {
+  F_CLOSE(stream->fs);
+  return 0;
 }
 
-
-PIXEL Proxy_GetPixel(uIMG* hIMG, INDEX x, INDEX y, INDEX z){
-    return UHI_ReadPixel(hIMG, (dim3) {x, y, z});
+PIXEL Proxy_GetPixel(uIMG* hIMG, INDEX x, INDEX y, INDEX z) {
+  return UHI_ReadPixel(hIMG, (dim3){x, y, z});
 }
 
-void Proxy_SetPixel(uIMG* hIMG, INDEX x, INDEX y, INDEX z, PIXEL value){
-    UHI_WritePixel(hIMG, (dim3) {x, y, z}, value);
+void Proxy_SetPixel(uIMG* hIMG, INDEX x, INDEX y, INDEX z, PIXEL value) {
+  UHI_WritePixel(hIMG, (dim3){x, y, z}, value);
 }
